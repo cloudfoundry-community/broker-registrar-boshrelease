afa9b04f86c35ce1f3d108d321c4be1e2f1b18cd
Benjamin Gandon <benjamin@gstack.io>
2018-09-26 14:48:53 +0200

    Escape BOSH properties for shell syntax + Fix potential quoting issues + Adopt the 'pipefail' option

--- jobs/broker-deregistrar/templates/run.sh
+++ jobs/broker-deregistrar/templates/run.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
+<%
+  require "shellwords"
+
+  def esc(x)
+      Shellwords.shellescape(x)
+  end
+%>
+
 exec 2>&1
 
 export PATH=$PATH:/var/vcap/packages/jq/bin
 export PATH=$PATH:/var/vcap/packages/cf-cli/bin
 
-set -eu
+set -eo pipefail -u
 
 <% cf = nil; if_link("cf-admin-user") { |link| cf = link } -%>
-CF_API_URL='<%= cf ? cf.p("api_url") : p("cf.api_url") %>'
-CF_ADMIN_USERNAME='<%= cf ? cf.p("admin_username") : p("cf.username") %>'
-CF_ADMIN_PASSWORD='<%= cf ? cf.p("admin_password") : p("cf.password") %>'
+CF_API_URL=<%= esc(cf ? cf.p("api_url") : p("cf.api_url")) %>
+CF_ADMIN_USERNAME=<%= esc(cf ? cf.p("admin_username") : p("cf.username")) %>
+CF_ADMIN_PASSWORD=<%= esc(cf ? cf.p("admin_password") : p("cf.password")) %>
 <% if cf -%>
 mkdir -p /var/vcap/sys/run
-cat > /var/vcap/sys/run/cf.crt <<EOF
+cat > /var/vcap/sys/run/cf.crt <<END_OF_CERT
 <%= cf.p("ca_cert") %>
-EOF
+END_OF_CERT
 export SSL_CERT_FILE=/var/vcap/sys/run/cf.crt
 <% end -%>
-CF_SKIP_SSL_VALIDATION=<%= p("cf.skip_ssl_validation") %>
+CF_SKIP_SSL_VALIDATION=<%= esc(p("cf.skip_ssl_validation")) %>
 
 <%
   servicebroker_name = p("servicebroker.name", nil)
   servicebroker_name ||= link("servicebroker").p("name")
 %>
-BROKER_NAME='<%= servicebroker_name %>'
+BROKER_NAME=<%= esc(servicebroker_name) %>
 
 echo "CF_API_URL=${CF_API_URL}"
 echo "CF_SKIP_SSL_VALIDATION=${CF_SKIP_SSL_VALIDATION}"
@@ -32,22 +40,18 @@ echo "CF_ADMIN_USERNAME=${CF_ADMIN_USERNAME}"
 echo "BROKER_NAME=${BROKER_NAME}"
 
 if [[ ${CF_SKIP_SSL_VALIDATION} == "true" ]]; then
-  cf api ${CF_API_URL} --skip-ssl-validation
+  cf api "${CF_API_URL}" --skip-ssl-validation
 else
-  cf api ${CF_API_URL}
+  cf api "${CF_API_URL}"
 fi
 
-cf auth \
-  ${CF_ADMIN_USERNAME} \
-  ${CF_ADMIN_PASSWORD}
+cf auth "${CF_ADMIN_USERNAME}" "${CF_ADMIN_PASSWORD}"
 
-BROKER_GUID=$(cf curl /v2/service_brokers\?q=name:${BROKER_NAME} | jq -r ".resources[0].metadata.guid")
-SERVICE_NAMES=($(cf curl /v2/services\?q=service_broker_guid:${BROKER_GUID} | jq -r ".resources[].entity.label"))
+BROKER_GUID=$(cf curl "/v2/service_brokers?q=name:${BROKER_NAME}" | jq -r ".resources[0].metadata.guid")
+SERVICE_NAMES=($(cf curl "/v2/services?q=service_broker_guid:${BROKER_GUID}" | jq -r ".resources[].entity.label"))
 
 for service_name in "${SERVICE_NAMES[@]}"; do
-  cf purge-service-offering $service_name -f
+  cf purge-service-offering "${service_name}" -f
 done
 
-cf delete-service-broker \
-  ${BROKER_NAME} \
-  -f
+cf delete-service-broker "${BROKER_NAME}" -f
--- jobs/broker-registrar/templates/run.sh
+++ jobs/broker-registrar/templates/run.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+<%
+  require "shellwords"
+
+  def esc(x)
+      Shellwords.shellescape(x)
+  end
+%>
+
 exec 2>&1
 
 export PATH=$PATH:/var/vcap/packages/jq/bin
 export PATH=$PATH:/var/vcap/packages/cf-cli/bin
 
-set -eu
+set -eo pipefail -u
 
 <% cf = nil; if_link("cf-admin-user") { |link| cf = link } -%>
-CF_API_URL='<%= cf ? cf.p("api_url") : p("cf.api_url") %>'
-CF_ADMIN_USERNAME='<%= cf ? cf.p("admin_username") : p("cf.username") %>'
-CF_ADMIN_PASSWORD='<%= cf ? cf.p("admin_password") : p("cf.password") %>'
+CF_API_URL=<%= esc(cf ? cf.p("api_url") : p("cf.api_url")) %>
+CF_ADMIN_USERNAME=<%= esc(cf ? cf.p("admin_username") : p("cf.username")) %>
+CF_ADMIN_PASSWORD=<%= esc(cf ? cf.p("admin_password") : p("cf.password")) %>
 <% if cf -%>
 mkdir -p /var/vcap/sys/run
 cat > /var/vcap/sys/run/cf.crt <<EOF
@@ -18,7 +26,7 @@ cat > /var/vcap/sys/run/cf.crt <<EOF
 EOF
 export SSL_CERT_FILE=/var/vcap/sys/run/cf.crt
 <% end -%>
-CF_SKIP_SSL_VALIDATION=<%= p("cf.skip_ssl_validation") %>
+CF_SKIP_SSL_VALIDATION=<%= esc(p("cf.skip_ssl_validation")) %>
 
 <%
   broker_url = p("servicebroker.url", nil)
@@ -35,18 +43,21 @@ CF_SKIP_SSL_VALIDATION=<%= p("cf.skip_ssl_validation") %>
     broker_password ||= broker.p("password")
   end
 %>
-BROKER_NAME='<%= broker_name %>'
-BROKER_URL='<%= broker_url %>'
-BROKER_USERNAME='<%= broker_username %>'
-BROKER_PASSWORD='<%= broker_password %>'
+BROKER_NAME=<%= esc(broker_name) %>
+BROKER_URL=<%= esc(broker_url) %>
+BROKER_USERNAME=<%= esc(broker_username) %>
+BROKER_PASSWORD=<%= esc(broker_password) %>
 
 function createOrUpdateServiceBroker() {
-  if [[ "$(cf curl /v2/service_brokers\?q=name:${BROKER_NAME} | jq -r .total_results)" == "0" ]]; then
+  local brokers_count
+  brokers_count=$(cf curl "/v2/service_brokers?q=name:${BROKER_NAME}" \
+      | jq -r .total_results)
+  if [[ "${brokers_count}" == "0" ]]; then
     echo "Service broker '${BROKER_NAME}' does not exist - creating broker"
-    cf create-service-broker ${BROKER_NAME} ${BROKER_USERNAME} ${BROKER_PASSWORD} ${BROKER_URL}
+    cf create-service-broker "${BROKER_NAME}" "${BROKER_USERNAME}" "${BROKER_PASSWORD}" "${BROKER_URL}"
   else
     echo "Service broker '${BROKER_NAME}' already exists - updating broker"
-    cf update-service-broker ${BROKER_NAME} ${BROKER_USERNAME} ${BROKER_PASSWORD} ${BROKER_URL}
+    cf update-service-broker "${BROKER_NAME}" "${BROKER_USERNAME}" "${BROKER_PASSWORD}" "${BROKER_URL}"
   fi
 }
 
@@ -58,22 +69,20 @@ echo "BROKER_URL=${BROKER_URL}"
 echo "BROKER_USERNAME=${BROKER_USERNAME}"
 
 if [[ ${CF_SKIP_SSL_VALIDATION} == "true" ]]; then
-  cf api ${CF_API_URL} --skip-ssl-validation
+  cf api "${CF_API_URL}" --skip-ssl-validation
 else
-  cf api ${CF_API_URL}
+  cf api "${CF_API_URL}"
 fi
 
-cf auth \
-  ${CF_ADMIN_USERNAME} \
-  ${CF_ADMIN_PASSWORD}
+cf auth "${CF_ADMIN_USERNAME}" "${CF_ADMIN_PASSWORD}"
 
 createOrUpdateServiceBroker
 
 cf service-access
 
-service_names=($(curl -s -H "X-Broker-Api-Version: 2.10" -u ${BROKER_USERNAME}:${BROKER_PASSWORD} ${BROKER_URL}/v2/catalog | jq -r ".services[].name"))
+service_names=($(curl -s -H "X-Broker-Api-Version: 2.10" -u "${BROKER_USERNAME}:${BROKER_PASSWORD}" "${BROKER_URL}/v2/catalog" | jq -r ".services[].name"))
 for service_name in "${service_names[@]}"; do
-  cf enable-service-access $service_name
+  cf enable-service-access "${service_name}"
 done
 
 cf service-access
